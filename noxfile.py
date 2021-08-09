7bb693617317cd207bee74b90cd545c778cc6c30
Mateus Otto <mateus.potto@usp.br>
2021-08-09 20:35:32 -0300

    add linting and preliminary tests

--- noxfile.py
+++ noxfile.py
@@ -1,1 +1,71 @@
 # /noxfile.py
+"""Nox sessions."""
+import tempfile
+from typing import Any
+
+import nox
+from nox.sessions import Session
+
+nox.options.sessions = "lint", "tests"
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    """Install packages constrained by Poetry's lock file."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8", "3.7"])
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session: Session) -> None:
+    """Lint using flake8."""
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-annotations",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+        "flake8-docstrings",
+        "darglint",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def black(session: Session) -> None:
+    """Run black code formatter."""
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def docs(session: Session) -> None:
+    """Build the documentation."""
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "sphinx", "sphinx-autodoc-typehints")
+    session.run("sphinx-build", "docs", "docs/_build")
