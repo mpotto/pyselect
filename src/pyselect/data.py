16b01382bbf9e853066768f36a524844c8601568
Mateus Otto <mateus.potto@usp.br>
2022-03-21 08:55:37 -0300

    add generator to local pnrg, expand function signatures, remove splitting function

--- src/pyselect/data.py
+++ src/pyselect/data.py
@@ -2,7 +2,7 @@
 """Data synthesizer for running experiments."""
 import numpy as np
 import torch
-from torch.distributions import MultivariateNormal
+from torch import Generator
 
 
 def make_regression_with_tensors(
@@ -11,8 +11,12 @@ def make_regression_with_tensors(
     n_informative: int = 10,
     bias: float = 0.0,
     noise: float = 1.0,
+    generator: Generator = torch.default_generator,
 ):
-    """Create a synthetic regression dataset. The covariates are sampled from a standard normal distribution. The response variable is a linear model with non-zero coefficients only for a subset of `n_informative` covariates plus a normally distributed noise.
+    """Create a synthetic regression dataset. The covariates are sampled from
+    a standard normal distribution. The response variable is a linear model
+    with non-zero coefficients only for a subset of `n_informative` covariates
+    plus a normally distributed noise.
 
     Args:
         n_samples: Number of samples in the dataset.
@@ -21,63 +25,53 @@ def make_regression_with_tensors(
         n_targets: Number of targets (response variables)
         bias: Bias of the linear model.
         noise: Standard deviation of the noise.
+        seed: integer random state for dataset generation.
 
     Returns:
         X: Design matrix.
         y: Response variable.
         coef: Coefficients of the linear model.
     """
-    X = torch.randn(n_samples, n_features)
+    X = torch.randn(n_samples, n_features, generator=generator)
     coef = torch.zeros(n_features)
-    relevant_feat = np.random.choice(n_features, n_informative, replace=False)
-    coef[relevant_feat] = torch.randn(n_informative)
-    y = X @ coef.unsqueeze_(-1) + noise * torch.randn(n_samples).unsqueeze(-1) + bias
+    relevant_feat = torch.multinomial(
+        torch.ones(n_features) / n_features, n_informative, generator=generator
+    )
+    coef[relevant_feat] = torch.randn(n_informative, generator=generator)
+    y = (
+        X @ coef.unsqueeze_(-1)
+        + noise * torch.randn(n_samples, generator=generator).unsqueeze(-1)
+        + bias
+    )
     return X, y, coef
 
 
-def gregorova_se1(train_size: int = 10 ** 3, test_size: int = 500):
+def gregorova_se1(
+    n_samples: int = 2000,
+    noise: float = 0.1,
+    generator: Generator = torch.default_generator,
+):
     """Gregorova *et al* article Synthetic Experiment 1."""
-    noise = 0.1
-    n_samples = train_size + 2 * test_size
-    X = torch.randn(n_samples, 18)
-    y = torch.sin(torch.square(X[:, 0] + X[:, 2])) * torch.sin(
+    X = torch.randn(n_samples, 18, generator=generator)
+    y_noise = torch.sin(torch.square(X[:, 0] + X[:, 2])) * torch.sin(
         X[:, 6] * X[:, 7] * X[:, 8]
-    )
-    y_noise = y.unsqueeze(-1) + noise * torch.randn(n_samples).unsqueeze(-1)
-
-    X_train = X[:train_size, :]
-    y_train = y_noise[:train_size]
-
-    y_train_mean = y_train.mean()
-
-    y_train = y_train - y_train_mean
-    X_val = X[train_size : train_size + test_size, :]
-    y_val = y_noise[train_size : train_size + test_size] - y_train_mean
-    X_test = X[train_size + test_size :, :]
-    y_test = y_noise[train_size + test_size :] - y_train_mean
-
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    ) + noise * torch.randn(n_samples, generator=generator)
+    y = y_noise.unsqueeze(-1)
+    return X, y
 
 
-def gregorova_se2(train_size: int = 10 ** 3, test_size: int = 500):
+def gregorova_se2(
+    n_samples: int = 2000,
+    noise: float = 0.1,
+    generator: Generator = torch.default_generator,
+):
     """Gregorova *et al* article Synthetic Experiment 2."""
-    noise = 0.1
-    n_samples = train_size + 2 * test_size
-    X = torch.randn(n_samples, 100)
-    y = torch.log(torch.square(torch.sum(X[:, 10:15], axis=1)))
-    y_noise = y.unsqueeze(-1) + noise * torch.randn(n_samples)
-
-    X_train = X[:train_size, :]
-    y_train = y_noise[:train_size]
-
-    y_train_mean = y_train.mean()
-
-    y_train = y_train - y_train_mean
-    X_val = X[train_size : train_size + test_size, :]
-    y_val = y_noise[train_size : train_size + test_size] - y_train_mean
-    X_test = X[train_size + test_size :, :]
-    y_test = y_noise[train_size + test_size :] - y_train_mean
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    X = torch.randn(n_samples, 100, generator=generator)
+    y_noise = torch.log(
+        torch.square(torch.sum(X[:, 10:15], axis=1))
+    ) + noise * torch.randn(n_samples, generator)
+    y = y_noise.unsqueeze(-1)
+    return X, y
 
 
 def jordan_se1(
@@ -85,16 +79,21 @@ def jordan_se1(
     n_features: int = 2,
     rho: float = 0.5,
     noise_level: float = 0.1,
+    generator: Generator = torch.default_generator,
 ):
     """Jordan *et al* article Synthetic Experiment 1."""
-    features_indices = np.arange(1, n_features + 1)
-    col_indices, row_indices = np.meshgrid(features_indices, features_indices)
-    power_matrix = np.abs(col_indices - row_indices)
+    features_indices = torch.arange(1, n_features + 1)
+    col_indices, row_indices = torch.meshgrid(
+        features_indices, features_indices, indexing="xy"
+    )
+    power_matrix = torch.abs(col_indices - row_indices)
     cov = torch.Tensor(rho ** power_matrix)
     loc = torch.zeros(n_features)
-    normal_dist = MultivariateNormal(loc, covariance_matrix=cov)
-    X = normal_dist.sample((n_samples,))
-    y_noise = X[:, 0] + noise_level * torch.randn(n_samples)
+
+    rng = np.random.default_rng(seed=generator.get_state().numpy())
+    X = torch.Tensor(rng.multivariate_normal(loc, cov, size=n_samples))
+
+    y_noise = X[:, 0] + noise_level * torch.randn(n_samples, generator=generator)
     y = y_noise.unsqueeze(-1)
     return X, y
 
@@ -104,34 +103,39 @@ def jordan_se2(
     n_features: int = 10,
     rho: float = 0.5,
     noise_level: float = 0.1,
+    generator: Generator = torch.default_generator,
 ):
     """Jordan *et al* article Synthetic Experiment 2."""
-    features_indices = np.arange(1, n_features + 1)
-    col_indices, row_indices = np.meshgrid(features_indices, features_indices)
-    power_matrix = np.abs(col_indices - row_indices)
+    features_indices = torch.arange(1, n_features + 1)
+    col_indices, row_indices = torch.meshgrid(
+        features_indices, features_indices, indexing="xy"
+    )
+    power_matrix = torch.abs(col_indices - row_indices)
     cov = torch.Tensor(rho ** power_matrix)
     loc = torch.zeros(n_features)
-    normal_dist = MultivariateNormal(loc, covariance_matrix=cov)
-    X = normal_dist.sample((n_samples,))
-    y_noise = X[:, 0] ** 3 + X[:, 1] ** 3 + noise_level * torch.randn(n_samples)
+
+    rng = np.random.default_rng(seed=generator.get_state().numpy())
+    X = torch.Tensor(rng.multivariate_normal(loc, cov, size=n_samples))
+
+    y_noise = (
+        X[:, 0] ** 3
+        + X[:, 1] ** 3
+        + noise_level * torch.randn(n_samples, generator=generator)
+    )
     y = y_noise.unsqueeze(-1)
     return X, y
 
 
-def jordan_se3(n_samples: int = 200, n_features: int = 10, noise_level: float = 0.1):
+def jordan_se3(
+    n_samples: int = 200,
+    n_features: int = 10,
+    noise_level: float = 0.1,
+    generator: Generator = torch.default_generator,
+):
     """Jordan *et al* article Synthetic Experiment 3."""
-    X = torch.randn((n_samples, n_features))
-    y_noise = X[:, 0] * X[:, 1] + noise_level * torch.randn(n_samples)
+    X = torch.randn((n_samples, n_features), generator=generator)
+    y_noise = X[:, 0] * X[:, 1] + noise_level * torch.randn(
+        n_samples, generator=generator
+    )
     y = y_noise.unsqueeze(-1)
     return X, y
-
-
-def train_val_test_split(X, y, train_size, val_size, test_size):
-    """Split dataset in train, validation and test parts."""
-    X_train = X[:train_size, :]
-    y_train = y[:train_size]
-    X_val = X[train_size : train_size + val_size, :]
-    y_val = y[train_size : train_size + val_size]
-    X_test = X[train_size + val_size : train_size + val_size + test_size, :]
-    y_test = y[train_size + val_size : train_size + val_size + test_size]
-    return X_train, y_train, X_val, y_val, X_test, y_test
